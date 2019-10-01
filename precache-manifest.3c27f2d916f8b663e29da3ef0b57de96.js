8cfde84e37dc3561f8b650be5e3def8ad9fbe703
nckv <nickvonh@gmail.com>
2019-10-01 15:00:07 -0400

    first push

---
+++ precache-manifest.3c27f2d916f8b663e29da3ef0b57de96.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fdb15a1fe05ea2e8c897754cafbecdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbad7abb33696eb031a4",
+    "url": "/static/css/main.cf18ab50.chunk.css"
+  },
+  {
+    "revision": "b0d7ef06365280963034",
+    "url": "/static/js/2.596da64a.chunk.js"
+  },
+  {
+    "revision": "fbad7abb33696eb031a4",
+    "url": "/static/js/main.905f288a.chunk.js"
+  },
+  {
+    "revision": "f0d5cf4a4ca5d4507843",
+    "url": "/static/js/runtime~main.c0a5f382.js"
+  },
+  {
+    "revision": "f40889caa25956ef4d27c2aadc4fbe13",
+    "url": "/static/media/Comfortaa-VariableFont:wght.f40889ca.ttf"
+  },
+  {
+    "revision": "27f813cadd3a3af891e01acf64272748",
+    "url": "/static/media/Handlee-Regular.27f813ca.ttf"
+  },
+  {
+    "revision": "49a7196253894c0336e804da0cafaffd",
+    "url": "/static/media/blur-baby.49a71962.jpg"
+  },
+  {
+    "revision": "f48808684d9be723330de6d9fdc5f1ca",
+    "url": "/static/media/logo-baby.f4880868.jpg"
+  },
+  {
+    "revision": "03be321f904fff3503ffa9a3487b6f05",
+    "url": "/static/media/paper-plane.03be321f.svg"
+  },
+  {
+    "revision": "f3b392b265bfc98770e197ce6f908d8a",
+    "url": "/static/media/smile-baby.f3b392b2.jpg"
+  },
+  {
+    "revision": "7a2b1a329d12cadc8729ff8d17f517f3",
+    "url": "/static/media/vid3.7a2b1a32.mp4"
+  }
+]);
